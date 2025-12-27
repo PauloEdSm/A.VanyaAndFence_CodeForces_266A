b4fb229afad46974c4ff1573033d14c753f65cf4
Paulo Eduardo da silva M <pauloeduardotroll9646@gmail.com>
2025-12-27 16:00:41 +0000

    final version concerning the exercise

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("fffff");
+     public static void main(String[] args) {
 
+         Scanner scanner = new Scanner(System.in);
+         int totWidth = 0;
+         int personHeightMeters;
+         int numPeople = scanner.nextInt();
+         int fenceHeight = scanner.nextInt();
 
-    }
- }
+         for (int j = 0; j < numPeople ; j++) {
+             personHeightMeters = scanner.nextInt();
+             totWidth += (fenceHeight<=personHeightMeters)?1:2;
+         }
+         System.out.println(totWidth);
+         scanner.close();
+     }
+}
